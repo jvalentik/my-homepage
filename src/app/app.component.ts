e1f8fb9b6a777b62ef4667801714498258d61d0b
Jan Valentik <jvalentik@sk.ibm.com>
2017-03-31 22:25:24 +0200

    Refactored to accommodate new layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
 
 import { Component, OnInit } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { MdIconRegistry } from '@angular/material';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor(private translate: TranslateService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.translate.setDefaultLang('en');
-    this.translate.use('en');
+
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,46 +6,24 @@ import { HttpModule, Http } from '@angular/http';
 import { AppComponent } from './app.component';
 import { MaterialModule } from '@angular/material';
 import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppRoutingModule } from './app-routing.module';
-import { HomeModule } from './home/home.module';
-import { NotfoundModule } from './notfound/notfound.module';
-import { ToolbarModule } from './toolbar/toolbar.module';
-import { SidenavModule } from './sidenav/sidenav.module';
-import { AboutModule } from './about/about.module';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-export function HttpLoaderFactory(http: Http) {
-  return new TranslateHttpLoader(http);
-}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule,
-    MaterialModule.forRoot(),
+    MaterialModule,
     FlexLayoutModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [Http]
-      }
-    }),
     CoreModule,
-    SharedModule,
     AppRoutingModule,
-    HomeModule,
-    NotfoundModule,
-    ToolbarModule,
-    SidenavModule,
-    AboutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
